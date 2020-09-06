bfb1217a5d18c5ddc74b94dd66fdac0e18e3e686
gugucon-hwan <gugucon2018@gmail.com>
2020-09-07 03:39:45 +0900

    뷰 애니메이션, 모달, 뷰엑스, 반응성, 서버 사이드렌더링, 웹팩

--- chapter6/vue-todo/src/App.vue
+++ chapter6/vue-todo/src/App.vue
@@ -1,9 +1,9 @@
 <template lang="html">
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+    <TodoInput v-on:addTodo="addTodo"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"></TodoList>
+    <TodoFooter v-on:removeAll="clearAll"></TodoFooter>
   </div>
 </template>
 
@@ -13,6 +13,33 @@ import TodoInput from './components/TodoInput.vue'
 import TodoList from './components/TodoList.vue'
 import TodoFooter from './components/TodoFooter.vue'
 export default {
+  data(){
+    return{
+      todoItems:[]
+    }
+  },
+  created(){
+    if(localStorage.length>0){
+      for(var i=0;i<localStorage.length;i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+  methods:{
+    clearAll(){
+      localStorage.clear();
+      this.todoItems=[];
+    },
+    addTodo(todoItem){
+      //로컬 스토리지에 데이터를 추가하는 로직
+      localStorage.setItem(todoItem,todoItem);
+      this.todoItems.push(todoItem);
+    },
+    removeTodo(todoItem,index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index,1);
+    }
+  },
   components:{
     'TodoHeader':TodoHeader,
     'TodoInput':TodoInput,
