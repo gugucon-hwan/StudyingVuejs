add879708aba5de596ac62c11a8db7862cfb43b2
gugucon-hwan <gugucon2018@gmail.com>
2020-09-09 17:10:13 +0900

    완료!

--- webpack-simple/src/App.vue
+++ webpack-simple/src/App.vue
@@ -1,60 +1,36 @@
-<template>
+<template lang="html">
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+     App 컴포넌트<br>
+     <input id="calendar">
   </div>
 </template>
 
 <script>
+//window.$ = window.jQuery = window.jquery = require('jquery');
+import $ from 'jquery';
+//import $ from 'jquery-ui';
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+window.$ = window.jQuery = require('jquery-ui');
+//window.$ = window.jQuery = require('moment');
+//Vue.use(BootstrapVue)
+//Vue.use(BootstrapVueIcons)
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+
+  name: 'App.vue',
+  created() {
+    // var version = $.ui ? $.ui.version || "pre 1.6" : 'jQuery-UI not detected';
+    console.log($.fn.jquery);
+    console.log($.ui.version);
+  },
+  mounted() {
+    $('#calendar').datepicker();
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style lang="css" scoped>
 </style>
